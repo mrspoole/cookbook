cba6f3e6ff0aff32ce480bed1d683642074d2f41
mrspoole <92892719+mrspoole@users.noreply.github.com>
2026-03-22 17:24:30 -0500

    Update flask-app.py

--- flask-app.py
+++ flask-app.py
@@ -1,29 +1,35 @@
 from flask import Flask, render_template, request
+#import(yoink) recipe scrapers library
 from recipe_scrapers import scrape_me
-
+#Stuff to make python work with html
 app = Flask(__name__)
 
 @app.route("/recipe")
+
 def recipe():
+    #Tells the scraper where to go
+    #Example "https://www.bbcgoodfood.com/recipes/strawberry-ice-cream"
     url = request.args.get("url")
     try:
+        #go git it(data)
         scraper = scrape_me(url)
+        #All da stuffs we need saved as variables
         return render_template("recipe.html",
             title = scraper.title(),
+            ingredients = scraper.ingredients(),
+            steps = scraper.instructions_list(),
             image = scraper.image(),
             total_time = scraper.total_time(),
-            yields = scraper.yields(),
-            ingredients = scraper.ingredients(),
-            instructions = scraper.instructions_list(),
+            servings = scraper.yields(),
             error = None
         )
     except Exception as e:
         return render_template("recipe.html",
             error = str(e),
             title = None,
+            ingredients = [],
+            steps = [],
             image = None,
             total_time = None,
-            yields = None,
-            ingredients = [],
-            instructions = []
+            servings = None,
         )
